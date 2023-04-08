31e662bb08a02445e5f1901e7d563d165adf8e66
Albert State <statealbert@gmail.com>
2023-04-08 09:17:20 -0400

    etape 1 fonctionnelle

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
-monapp = Flask(__name__)
+app = Flask(__name__)
 
 
-@monapp.route("/")
+@app.route("/")
 def fonction_page_bonjour():
     return render_template("formulaire.html")
+
+if __name__ == '__main__':
+    app.run()
