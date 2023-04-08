c55570835a4dcd11bda23cf8670eaed132da751c
Albert State <statealbert@gmail.com>
2023-04-08 13:40:52 -0400

    version finale

--- main.py
+++ main.py
@@ -1,44 +1,27 @@
+## Albert State
+## 2046464
+## TP2
+############################################
+
 from flask import Flask
 from flask import render_template
 from flask import request
 
 app = Flask(__name__)
 
-
+####################formulaire pour rentrer les notes et les noms des etudiants####################
 
 @app.route("/")
 def fonction_page_bonjour():
     return render_template("formulaire.html")
 
 
-
-
-@app.route("/submit_form")
-def ecriture():
-    var_prenom = request.args.get("prenom")
-    var_nom = request.args.get("nom")
-    var_numero = request.args.get("numero")
-    var_anglais = request.args.get("anglais")
-    var_math = request.args.get("math")
-    var_francais = request.args.get("francais")
-    var_sciences = request.args.get("sciences")
-    var_histoire = request.args.get("histoire")
-    
-    emplacement = "data.txt"
-    
-    f = open(emplacement, "a")
-    f.write(f"Prenom: {var_prenom}\n")
-    f.write(f"Nom: {var_nom}\n")
-    f.write(f"Numero etudiant: {var_numero}\n")
-    f.write(f"Notes de l'etudiant: {var_anglais}, {var_math}, {var_francais}, {var_sciences}, {var_histoire}\n")
-
-        
-        
-    return render_template("tableauDeBord.html", prenom=var_prenom, nom=var_nom, numero=var_numero, anglais=var_anglais, math=var_math, francais=var_francais, sciences=var_sciences, histoire=var_histoire)
+####################tableau de bord pour afficher les noms des etudiants####################
+####################Le tableau de bord gere aussi l'ecriture dans le fichier####################
 
 @app.route("/tableauDeBord")
 def tableauDeBord():
-    
+####################ecriture dans le fichier####################
     var_prenom = request.args.get("prenom")
     var_nom = request.args.get("nom")
     var_numero = request.args.get("numero")
@@ -57,7 +40,7 @@ def tableauDeBord():
     f.write(f"Notes de l'etudiant: {var_anglais}, {var_math}, {var_francais}, {var_sciences}, {var_histoire}\n")
     
     
-    
+####################lecture du fichier####################
     emplacement = "data.txt"
     f = open(emplacement, "r")
     file = f.readlines()
@@ -72,6 +55,9 @@ def tableauDeBord():
     return render_template("tableauDeBord.html", noms=noms)
 
 
+
+#################### afficher les notes d'un etudiant selectionné a partir du tableau de bord ####################
+
 @app.route("/afficherNotes", methods=["POST"])
 def afficherNotes():
     
@@ -82,6 +68,7 @@ def afficherNotes():
     file = f.readlines()
     
     notes = {}
+#################### lecture du fichier et recherche de l'information utile ####################
     
     for i in range(len(file)):
         if "Nom: " + nom in file[i]:
